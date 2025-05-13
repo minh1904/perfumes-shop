50243f08e64dc829e02b48d86725043f7ffeaaba
Văn Minh <kenmoster19@gmail.com>
2025-05-13 18:08:04 +0700

    fix lai hero section

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/ui/landingpage/Hero';
+import Pledge from '@/components/ui/landingpage/Pledge';
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Pledge />
     </div>
   );
 }
---
+++ src/components/ui/landingpage/Pledge.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Pledge = () => {
+  return <div className=""></div>;
+};
+
+export default Pledge;
