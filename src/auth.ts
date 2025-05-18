b9b4673c3a8be0712da8de58fd6bf4ddfe314e3f
Văn Minh <kenmoster19@gmail.com>
2025-05-18 23:48:34 +0700

    fix

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
-import { db } from './db/db';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
